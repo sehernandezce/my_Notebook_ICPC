0be3b141f5592be40d7d78b64f1fd775d6c7b824
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-07-30 11:52:34 -0500

    Add files via upload

---
+++ math/Permutation.cpp
@@ -0,0 +1,14 @@
+
+// Algorithm or Logic: Permutation
+// Complexity: ?
+// Permutation through indices
+// Following permutation lexicographically.
+
+void permut(ll n){
+    for(int i=0; i<n; i++) arr_i[i]=i; // Create Indexes
+    
+    do{
+        //process permutation
+  
+    }while(next_permutation(arr_i,arr_i+n)); // next_permutation(vec.begin(),vec.end());
+}
