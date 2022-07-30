27a2a73de03aa0897bb638c98a085e8a8102413d
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-07-30 13:33:39 -0500

    Add files via upload

---
+++ math/primes_criba_de_eratóstenes.cpp
@@ -0,0 +1,28 @@
+//Algorithm or Logic: Sieve of Eratosthenes (criba de eratóstenes algoritmo)
+//Complexity: O(n log(log n))
+//
+
+vector <ll> prime;
+void prim(ll n){ // primes with n >= 2
+    ll num[n+5];
+    for(ll i=2; i<=n; i++) num[i]=i;
+
+    ll i=2;
+    while(i*i<n){ // i*i<n 2 to sqrt(n) // i<=n for all primes before n
+        if(num[i]!=-1){
+            prime.push_back(i);
+            for(ll j=i; j <= n/i; j++){
+                num[i*j]=-1;
+            }
+        }
+        i++;
+    }
+}
+
+/*
+    int n;
+    cin >> n;
+    prim(n);
+    for(auto ele: prime) cout << ele << " ";
+    cout << endl;
+*/
