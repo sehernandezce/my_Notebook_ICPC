c939a19c0af338c3788f4139f0166f59ed8a7284
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-07-29 20:56:17 -0500

    Add files via upload

---
+++ sorted.cpp
@@ -0,0 +1,16 @@
+//Algorithm or Logic: Sort 
+//Complexity: O(log N)
+
+const ll MAXN=9e17+10;
+ll arr[MAXN];
+vector <ll> vec;
+
+void sorted(){
+        // Sort from largest to smallest
+    // For vectors 
+    sort(vec.begin(),vec.end()); // sort(vec.rbegin(),vec.rend());
+
+    //For arrays
+    ll n=sizeof(arr)/sizeof(arr[0]); // Get the size
+    sort(arr,arr+n); //reverse(arr,arr+n);
+}
