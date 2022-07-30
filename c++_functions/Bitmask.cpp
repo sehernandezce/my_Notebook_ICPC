903e8432d1c9ffa320f7489b3ab60828f89b4fbb
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-07-30 09:57:03 -0500

    Add files via upload

---
+++ c++_functions/Bitmask.cpp
@@ -0,0 +1,20 @@
+// Algorithm or Logic: Bitmask/Bitmasking
+// Complexity: O(n) 
+
+void bitmask(){
+    ll n,b;
+    n << b; // n <<= b; n / 2^b
+    n >> b; // n >>= b; n * 2^b   
+}
+
+// Note
+// It is useful for when we have several representations but they can only be given the status of is or is not.
+// Allows for better space management.
+
+// More info: https://www.youtube.com/watch?v=h6O6qVw0e5M&ab_channel=makigas%3Atutorialesdeprogramaci%C3%B3n
+// https://youtu.be/oL5bmRUbAJ4
+// mod(%) O(n^3)
+// Div(/) *Mult O(n^2)  
+// bitwise O(n) 
+// Operator and(&) or(|) xor(^) not op(~)
+// left shift(<<) right shift op(>>)
