8b2301f7556c8a1e07b2e7b11e0ad4d112e876fe
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-08-13 10:46:37 -0500

    Update main.cpp

--- C++_main/main.cpp
+++ C++_main/main.cpp
@@ -8,24 +8,22 @@ More info:
 #include <bits/stdc++.h>
 using namespace std;
 
-//#define int long long int
 #define F first 
 #define S second
 #define PB push_back
-#define MP make_pair
-#define MAX LLONG_MAX
-#define MIN LLONG_MIN
 #define endl '\n'
+//#define int long long int
+//#define MP make_pair
+//#define MAX LLONG_MAX
+//#define MIN LLONG_MIN
 //#define ITE(i,a,b) for(int i = a; i <= n; i++)
 
-typedef long long ll;
-//typedef int td; // Define type data int as td
+typedef long long ll; //typedef int td; // Define type data int as td
 
-using ii=pair<int,int>;
+//using ii=pair<int,int>;
 
-const int INF= 1*1e9+10;
-
-const int MAXN=1e5+5;
+const int oo = 1*1e9+10; // INFN
+const int MAXN = 1e5+5;
 
 void solv(){
     int n;
@@ -35,14 +33,11 @@ void solv(){
 
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    //cout.tie(0);
+    cin.tie(0), cout.tie(0);
 
     #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-        //freopen("input.txt", "r", stdin);
-        //freopen("output.txt", "w", stdout);
+        freopen("input.txt", "r", stdin); //freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout); //freopen("output.txt", "w", stdout);       
     #endif
     
     //solv(); 
