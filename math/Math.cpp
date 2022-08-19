234f7ec81429bd245058ff73d02ab37b806d9e37
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-08-19 13:07:57 -0500

    Add files via upload

---
+++ math/Math.cpp
@@ -0,0 +1,11 @@
+//MATH
+//combinatorics, permutations and others
+
+//Algorithm or Logic: Number of pairs given n elements
+//Complexity: O(1)
+//
+
+ll sumN(ll n){
+    n--;
+    return n*(n+1)/2;
+}
