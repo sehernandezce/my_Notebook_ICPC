adf59aee9a121f5ae616265254eb71834d3f11ba
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-08-20 18:30:45 -0500

    Update Binary_Search.cpp

--- search_Algorithms/Binary_Search.cpp
+++ search_Algorithms/Binary_Search.cpp
@@ -3,18 +3,18 @@
 
 // version 1
 ll binarySearch(ll size, ll target){
-    ll l=0,r=size-1;
-    ll ans=-1;
+    ll l=0,r=size-1,ans=-1;
     while (l<=r)
     {   
         ll mid = l + ((r-l) >> 1);
-        if(data[mid] >= target) r=mid-1, ans=mid;
+        if(dat[mid] == target) return mid;
+        if(dat[mid] > target) r=mid-1;
         else l=mid+1;
     }
     return ans; 
 }
 
-// Version 2
+// ll i=lower_bound(v.begin(), v.end(), value) - v.begin() ; array -> ll i=lower_bound(dat,dat+n,k) - dat
 ll binarySearch(ll size, ll value){ // N=size; search -> value O(log N)
     ll l=0,r=size-1;
     while (l<=r)
