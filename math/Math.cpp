caa72485da67c6a6bd409057f3784ad0cd527d21
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-08-19 21:07:28 -0500

    Update Math.cpp

--- math/Math.cpp
+++ math/Math.cpp
@@ -9,3 +9,18 @@ ll sumN(ll n){
     n--;
     return n*(n+1)/2;
 }
+
+//Algorithm or Logic: sum of the digits of the number n
+//Complexity: O(?)
+//
+
+ll sumS(ll x)
+{
+    ll sum = 0;
+    while(x){
+        sum += (x%10);
+        x/=10;
+    }
+
+    return sum;
+}
