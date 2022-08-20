ecd323ecb2cd9945d41e98407774af9068cef0ed
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-08-20 03:58:08 -0500

    Update Math.cpp

--- math/Math.cpp
+++ math/Math.cpp
@@ -22,3 +22,27 @@ ll sumS(ll x){
     }
     return sum;
 }
+
+//Algorithm or Logic: Find sqrt(x) with some precision
+//Complexity: O(nlogn)
+//
+
+typedef long double ll;
+
+ll ok(ll mid, ll k){ // mid^k
+  ll ans=1;
+  while(k--){
+    ans*=mid;
+  }
+  return ans;
+}
+
+ll bs (ll x, ll err, ll k){
+    ll l = 0.0, r = x;
+    while(r - l > err){
+        ll mid = l + ((r-l)/2);
+        if (ok(mid,k) >= x) r = mid; 
+        else l = mid;
+    }
+    return l + ((r-l)/2);
+}
