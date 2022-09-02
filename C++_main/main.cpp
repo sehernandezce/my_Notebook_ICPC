57071742f5ed37b6f6be0c2369692dd94d141323
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2022-09-02 10:46:01 -0500

    Update main.cpp

--- C++_main/main.cpp
+++ C++_main/main.cpp
@@ -12,12 +12,12 @@ More info:
 #define sc second
 #define mt make_tuple
 #define pb push_back
-#define all(s) s.begin(),s.end()
+#define all(s) s.begin(),s.end() 
 #define allr(s) s.rbegin(),s.rend()
 #define sz size
 #define len length
 #define forn(i,n) for(ll i = 0; i < (ll)(n); i++) // for, input, output
-#define foran(i,a,n) for(ll i = a; i < (ll)(n); i++)
+#define foran(i,a,n) for(ll i = (ll)(a); i < (ll)(n); i++)
 #define IO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define precise(n,k) fixed << setprecision(k) << n
 #define endl '\n'
@@ -29,52 +29,39 @@ More info:
 using namespace std;
 
 typedef long long ll;
-typedef complex<ll> P;
+typedef double dd; 
+typedef complex<ll> cc;
 
 using ii=pair<ll,ll>;
 using iii=tuple<ll,ll,ll>;
 
 const ll oo = 1e18+10; // 1e9+10;
-const double PI = 3.141592653589793;
+const dd PI = 3.141592653589793;
 
 void rwArchive();
-void tests();
 //  BEGIN
+const ll MAXN = 1e4+5;
 
-const ll MAXN = 1e5+10;
-
-void solv(){
-    ll n;
-    cin >> n;
-    cout << n;
-    
-    cout << endl;    
-}
 
 int main(){
-    IO;
-    rwArchive();  
-    //Begin 
+    IO; 
+    rwArchive();
+    //Begin
 
-    solv(); 
-    //tests();
+    ll n;
+    cin >> n;
+    cout << n << endl;
     
-
+    //End
     return 0;
 }
 
 //  END
 void rwArchive(){
     #ifndef ONLINE_JUDGE  
-        freopen("input.txt", "r", stdin); 
-        freopen("output.txt", "w", stdout);
+       freopen("input.txt", "r", stdin); 
+       freopen("output.txt", "w", stdout);  
     #endif
-}
-
-void tests(){
-    ll t;
-    cin >> t;
-    forn(i,t) solv();
 }
 
 //made by Sebastian
