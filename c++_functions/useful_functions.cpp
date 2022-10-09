b8f43a59baf53701f761b54033d2f039344b8658
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2022-10-08 21:50:20 -0500

    Update useful_functions.cpp

--- c++_functions/useful_functions.cpp
+++ c++_functions/useful_functions.cpp
@@ -84,7 +84,13 @@ priority_queue<int,vector<int>,greater<int>> que; // priority_queue<int, vector<
 // If we want to create a priority queue that supports finding and removing the smallest element
 queue <ll> que;
 
-//DEQUEUE
+//DEQUE
+deque<int> d;
+d.push_back(5); // [5]
+d.push_back(2); // [5,2]
+d.push_front(3); // [3,5,2]
+d.pop_back(); // [3,5]
+d.pop_front(); // [5]
 
 //STACK
  
