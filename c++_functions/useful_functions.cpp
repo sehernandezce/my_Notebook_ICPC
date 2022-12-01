fc31d2f9854b2691e65766031ff23394b3054391
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2022-12-01 14:15:14 -0500

    Update useful_functions.cpp

--- c++_functions/useful_functions.cpp
+++ c++_functions/useful_functions.cpp
@@ -1,6 +1,6 @@
 // C++ useful functions
 //g++ -o main main.cpp -Wall -Wextra -Wshadow
-//g++ -DLOCAL -std=c++11 -g -O2 -Wconversion -Wshadow -Wall -Wextra -D_GLIBCXX_DEBUG -c main main.cpp
+//g++ -DLOCAL -std=c++11 -g -O2 -Wconversion -Wshadow -Wall -Wextra -D_GLIBCXX_DEBUG -o "main" "main.cpp"
 //./main input.txt output.txt
 //In linux
 //g++ -DLOCAL -std=c++11 -g -O2 -Wconversion -Wshadow -Wall -Wextra -D_GLIBCXX_DEBUG -o "%e" "%f"
