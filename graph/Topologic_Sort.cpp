3be2054f8a4d3b7d9fec0b70dedbb515061805ed
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-02-26 09:50:37 -0500

    Update Topologic_Sort.cpp

--- graph/Topologic_Sort.cpp
+++ graph/Topologic_Sort.cpp
@@ -2,10 +2,12 @@
 //Complexity: 
 vector<int> topSort(vector<vector<int>>& g, vector<int>& inGrade) {
     int n = (int)inGrade.size();
+    int unique = 1;
     vector<int> topSorted;
     queue<int> q;
-    for(int i = 0; i < n; i++) if(inGrade[i] == 0) q.push(i);
+    for(int i = 1; i <= n; i++) if(inGrade[i] == 0) q.push(i);
     while(q.size()){
+        //if(q.size() > 1) unique = 0;
         int node = q.front(); q.pop();
         topSorted.push_back(node);
         for(int y: g[node]) if(--inGrade[y] == 0) q.push(y);
@@ -16,5 +18,5 @@ vector<int> topSort(vector<vector<int>>& g, vector<int>& inGrade) {
 }
 
 /*
-adj[i].push_back(k); inGrade[i]++;
+adj[i].push_back(k); inGrade[k]++;
 */
