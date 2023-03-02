8358b3102ae1e1e331415e49c4ba12e9193325e4
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-03-02 18:25:21 -0500

    Add files via upload

---
+++ graph/Successor_paths.cpp
@@ -0,0 +1,21 @@
+//Algorithm or logic: Successor paths/ functional graphs
+// Complexity: O(Nlogk) queries: log(k)
+int memo[N][31];
+int succ(int x, int i){ // 2^i
+    if(i == 0) return memo[x][0];
+    int &r = memo[x][i];
+    if(r == 0){
+        r = succ(succ(x,i-1),i-1);
+    }
+    return r;
+}
+
+// succ(n,30); // log2 (1e9) + 1
+/*
+        int ans = x;
+        for(int i = (int)(log2(k)+1); i>=1; i--){
+            if(k & (1 << (i-1))){
+                ans = succ(ans,i-1);
+            }
+        }
+*/
