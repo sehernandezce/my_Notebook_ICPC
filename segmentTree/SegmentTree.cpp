3e2a7e4778b2bcda03222ef2f24d9f8a757bfae3
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-03-11 12:44:12 -0500

    Update SegmentTree.cpp

--- segmentTree/SegmentTree.cpp
+++ segmentTree/SegmentTree.cpp
@@ -75,4 +75,40 @@ struct segTree{
     }
     void DoUpdate(int ind, T valNew){update(1,0,n-1,ind-1,valNew);}
 };
-//
+////// SEGMENT TREE
+typedef long long T;
+T segTree[4*N];
+ll data[N];
+
+T mrg(T data1, T data2){
+    return data1+data2;
+}
+
+void update(T nodeP, int l, int r, T newV, int posV){
+    if(l == r){
+        segTree[nodeP] = newV; 
+        return;
+    }
+    int mid = l + ((r-l)>>1);
+    T nodeL = (nodeP*2), nodeR = (nodeP*2)+1; 
+    if(posV > mid) update(nodeR, mid+1, r, newV, posV);
+    else update(nodeL, l, mid, newV, posV);
+    segTree[nodeP] = mrg(segTree[nodeL],segTree[nodeR]);
+    return;
+}
+
+T query(T nodeP, int l, int r, int i, int j){
+    if(l >= i && r <= j) return segTree[nodeP];
+    if(l > j || r < i)  return -oo;
+
+    int mid = l + ((r-l)>>1);   
+    T nodeL = (nodeP*2), nodeR = (nodeP*2)+1; 
+    T dataL = query(nodeL, l, mid, i, j), dataR = query(nodeR, mid+1, r, i, j);
+    
+    if(dataL == -oo) return dataR;
+    if(dataR == -oo) return dataL;
+
+    return  mrg(dataL, dataR);
+}
+// update(1,0,n-1, newV, posV)
+// query(1,0,n-1, i, j) 
