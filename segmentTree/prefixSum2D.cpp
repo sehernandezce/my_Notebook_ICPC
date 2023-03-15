942c36f9bc45ac019e5d2fcc90a7b515ac9269b5
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-03-14 21:10:29 -0500

    Update prefixSum2D.cpp

--- segmentTree/prefixSum2D.cpp
+++ segmentTree/prefixSum2D.cpp
@@ -1,7 +1,9 @@
 //Algorithm or logic: PrefixSum 2D
 //Complexity: O(N*M)
 
-ll pref2D[1005][1005]; ll val[1005][1005];
+//ll pref2D[1005][1005]; ll val[1005][1005];
+vector<vector<ll>> pref2D(1005, vector<ll>(1005));
+vector<vector<ll>> val(1005, vector<ll>(1005));
 for(int i =0; i<=1000; i++){for(int j =0; j<=1000; j++){ pref2D[i][j] = 0; val[i][j] = 0;}}
 	for(int i = 0; i<n; i++){
             ll h,w; cin >> h >> w;
