0e0067ac0b212bf9220c676f064e3c2ef70da6fe
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-03-22 22:22:32 -0500

    Update useful_functions.cpp

--- c++_functions/useful_functions.cpp
+++ c++_functions/useful_functions.cpp
@@ -95,7 +95,7 @@ using namespace __gnu_pbds;
 
 template<typename T>
 using ordered_set = tree<T, null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-ordered_set orset;
+ordered_set<int> orset;
 
 orset.find_by_order();
 orset.order_of_key();
