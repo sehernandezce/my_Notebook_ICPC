c3df701859ddbcdbbfdb126134785716b8a6df64
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-04-03 00:27:53 -0500

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,48 @@
+//Algorithm or logic: Tree
+//Complexity: O(n)
+
+//Distance from x to any node dfs(x,-1)
+int dis[N];
+void dfs(int u, int bef){
+    if(bef != -1) dis[u] = dis[bef] + 1;    
+    for(auto& v: adj[u]){
+        if(v == bef) continue;
+        dfs(v,u);
+    } 
+}
+
+//Count nodes of Subtree
+int cnt[N];
+void dfs2(int u, int bef){
+    cnt[u] = 1;
+    for(auto& v: adj[u]){
+        if(v == bef) continue;
+        dfs2(v, u);
+        cnt[u] += cnt[v];
+    }
+}
+
+//k-th Ancestors from node x
+int memo[N][31];
+int anc(int x, int k){
+    if(k == 0) return memo[x][k]; 
+    int &r = memo[x][k];
+    if(r == -1){
+        r = anc(anc(x,k-1), k-1);
+    }
+    return r;
+}
+/*
+memset(memo, -1, sizeof memo);
+memo[0][0] = 0;
+memo[1][0] = 0;
+
+anc(n,30);
+int ans = x;
+        for(int i = 30; i >= 0; i--){
+            if(k & (1<<i)){
+                ans = anc(ans, i);
+            }
+        }
+        if(ans == 0) ans = -1;
+*/
