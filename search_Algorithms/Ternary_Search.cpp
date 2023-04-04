63f5eecdbd9b4b6c6a607cf4fd87fd1412d72de2
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-04-04 09:20:33 -0500

    Update Ternary_Search.cpp

--- search_Algorithms/Ternary_Search.cpp
+++ search_Algorithms/Ternary_Search.cpp
@@ -3,13 +3,13 @@
 
 dd SearchTer(dd f[], double l, double r, dd err){
     bool b=true;
-    while(abs(r-l) > err) //
+    while(abs(r-l) > err) //lo <= hi
     {   
         double m1 =  l + (r - l) / 3; // ll mid = lo + ((hi - lo) >> 1); 
         double m2 = r - (r - l) / 3; // ll mid1 = mid + 1;
 
         if(f[m1] >= f[m2]){ // max >> f[m1] < f[m2]
-            l = m1; // lo = mid +1;
+            l = m1; // lo = mid +1; ans = mid;
         }else{
             r = m2; //hi = mid - 1;
         }
