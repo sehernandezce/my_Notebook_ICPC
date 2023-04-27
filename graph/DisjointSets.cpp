05af933f996843313ad19ccea69912bb9fb854dc
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-04-26 20:14:15 -0500

    Update DisjointSets.cpp

--- graph/DisjointSets.cpp
+++ graph/DisjointSets.cpp
@@ -26,3 +26,49 @@ struct UnionFind{
     }
 };
  
+
+// Union find Oman
+int f[N], len[N];
+int find(int x){ // log2(n)
+    if(x == f[x]) return x;
+    return f[x] = find(f[x]);
+}
+void merge(int a, int b){
+    a = find(a);
+    b = find(b);
+    ig(a == b) return;
+    if(len[a] > len[b]) swap(a,b); // En a queda el menor len
+    len[b] += len[a];
+    f[a]=b;
+    return;
+}
+
+// Union find Oman, add, size, undo
+int f[N], len[N];
+stack<int> st;
+int find(int x){ // log2(n)
+    if(x == f[x]) return x;
+    return find(f[x]);
+}
+void merge(int a, int b){
+    a = find(a);
+    b = find(b);
+    if(a == b){
+        st.push(-1);
+        return;
+    }
+    if(len[a] > len[b]) swap(a,b); // En a queda el menor len
+    len[b] += len[a];
+    f[a]=b;
+    st.push(a);
+    return;
+}
+void undo(){
+    int u = st.top(); st.pop();
+    if(a != -1){
+        int b = f[a];
+        f[a] = a;
+        len[b] -= len[a];
+    }
+    return;
+}
