5f2ac596129ce89bdb0e6724dbb18e9d98baecd3
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2023-08-27 23:03:22 -0500

    Update Kosaraju.cpp

--- graph/Kosaraju.cpp
+++ graph/Kosaraju.cpp
@@ -8,7 +8,7 @@ vector<int> vis1(N, 0);
 void dfs1(int u, vector<vector<int>>& adj){
     vis1[u] = 1;
     for(auto& v: adj[u]) if(!vis1[v]) dfs1(v, adj);
-    listNode.push_back(u);
+    listNode.push_back(u); // tiempo de salida
 }
 
 int numSCCC = 0;
@@ -27,15 +27,25 @@ void dfs(int u, vector<vector<int>>& adj){
 }
 
 void kosaraju(int n){
+    // Permite encontrar el orden topologico de las componentes
     for(int i = 1; i <= n; i++){
         if(!vis1[i]) dfs1(i, adjOri);
     }
-    
     reverse(all(listNode));
+
+    //El nodo con mayor tiempo de salida sera el nodo representante de esa componentes.
     for(auto& i: listNode){
         if(!vis2[i]) {
             numSCCC++;
             dfs2(i, adjInv);
+        }
+    }
+
+    // Nuevo grafo DAG, formado por la componentes. 
+    vector<vector<int>> nwadj(numSCCC+5);
+    for(int i = 1; i <= n; i++){
+        for(int j: adj[i]){
+            if(idc[i] != idc[j]) nwadj[dic[i]].pb(idc[j]);
         }
     }
 }
