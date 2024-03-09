89905570f0112310821a72fe14ad8aa41b87051b
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2024-03-09 12:06:35 -0500

    Update useful_functions.cpp

--- c++_functions/useful_functions.cpp
+++ c++_functions/useful_functions.cpp
@@ -26,6 +26,7 @@ Long double es distinto a double
 Evitar el uso de double o funciones que retornen este tipo de dato
 NO usar .count para saber si existe en un multiset !!!!!
 Para corregir el error de presicion de una sqrt ll rx = (x >= 36 ? sqrt(x)-5: 1); while (rx*rx<x) ++rx;
+El costo de borrar en un map es alto 
 
 */
 
