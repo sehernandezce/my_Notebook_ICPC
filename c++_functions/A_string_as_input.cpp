39b56d018e9b289e945183bd55453843b9973577
Sebastián Hernández <63028794+sehernandezce@users.noreply.github.com>
2024-05-12 19:00:07 -0500

    Update A_string_as_input.cpp

--- c++_functions/A_string_as_input.cpp
+++ c++_functions/A_string_as_input.cpp
@@ -11,23 +11,3 @@ void s_cin(){
         // process x
     }
 }
-
-/*
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    string line;
-    getline(cin, line);
-    cout << line << endl;
-    replace(line.begin(),line.end(),"."," "); // para remplazar . por espacios.
-    stringstream ss(line);
-
-    int x;
-    while (ss >> x){ // mientras haya elementos sacar elementos
-        cout << x << endl;
-    }
-    
-}
-*/
